92aa6f4cf661d46d95c9cd3b63f4332af683182d
codingnirvana <1348422+codingnirvana@users.noreply.github.com>
2021-08-26 14:18:52 +0530

    Making changes to deploy script to copy the abi to frontend

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,7 +9,27 @@ async function main() {
     await faucet.deployed();
     console.log(`Ethereum faucet deployed to address: ${faucet.address}`);
 
+    // We also save the contract's artifacts and address in the proper directory
+    saveFrontendFiles(faucet);
 }
+
+function saveFrontendFiles(faucet) {
+    const fs = require("fs");
+    const contractsDir = __dirname + "/../src/abis";
+  
+    if (!fs.existsSync(contractsDir)) {
+      fs.mkdirSync(contractsDir);
+    }
+  
+    const FaucetArtifact = artifacts.readArtifactSync("EthereumFaucet");
+  
+    fs.writeFileSync(
+      contractsDir + "/EthereumFaucet.json",
+      JSON.stringify(FaucetArtifact, null, 2)
+    );
+}
+
+
 
 main()
     .then(() => process.exit(0))
